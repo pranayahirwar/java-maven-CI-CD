3b6a87f1ea4caed734887a30b1389ede83dab392
Pranay Ahirwar <56798886+pranayahirwar@users.noreply.github.com>
2022-12-12 17:18:17 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def devStep() {
+    echo "This is build process"
+}
+
+def  testStep() {
+    echo "Here testing take place"
+}
+
+def beforProd() {
+    echo "Final rechecking before Production"
+    echo "You have chosen ${params.VERSION} during build"
+}
+
+
+return this
